c8e7fe1404d4dc698385e6bf05becbb84492733f
saivamsivarma <saivamsivarma6@gmail.com>
2020-05-23 21:38:46 +0530

    updated

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { faSlack,faGithub,faInstagram,faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { ParticlesConfig } from '../particles-config';
 
 declare let particlesJS: any;
 
@@ -18,18 +17,11 @@ export class WelcomeComponent implements OnInit {
   constructor() { }
 
   public ngOnInit(): void {
-    this.invokeParticles();
-  }
-
-  public invokeParticles(): void {
-    particlesJS('particles-js', ParticlesConfig, function() {});
   }
 
   Aboutme()
   {
     document.getElementById('about-me').scrollIntoView({behavior:'smooth'}); 
-  }
-  
-    
+  }  
 
 }
